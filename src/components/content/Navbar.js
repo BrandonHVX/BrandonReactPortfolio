5929ae1e829284ead9e48dc200a2a949520c0224
BrandonHVX <brandongines@gmail.com>
2019-10-28 01:04:00 -0400

    jumbo

--- src/components/content/Navbar.js
+++ src/components/content/Navbar.js
@@ -1,64 +1,49 @@
-import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAlignLeft } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAlignLeft } from "@fortawesome/free-solid-svg-icons";
 import {
-  Navbar,
-  NavDropdown,
-  Button,
   Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
   Nav,
-  NavItem
-} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 export default props => {
-  const [isOpen, setOpen] = useState(false)
-  const toggle = () => setOpen(!isOpen)
+  const [isOpen, setOpen] = useState(false);
+  const toggle = () => setOpen(!isOpen);
 
   return (
-    <Navbar className="mobile-nav" collapseOnSelect bg="dark" expand="lg">
-      Hello
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <Nav.Link tag={Link} to={'/page-1'}>
-              About
-            </Nav.Link>
-          </NavItem>
-          <NavItem>
-            <NavDropdown title="Projects" id="collasible-nav-dropdown">
-              <NavDropdown.Item tag={Link} to={'/webdev'}>
-                WebDev
-              </NavDropdown.Item>
-              <NavDropdown.Item tag={Link} to={'/video'}>
-                Video
-              </NavDropdown.Item>
-              <NavDropdown.Item tag={Link} to={'/design'}>
-                Design
-              </NavDropdown.Item>
-              <NavDropdown.Item tag={Link} to={'/motion'}>
-                Motion
-              </NavDropdown.Item>
-            </NavDropdown>
-          </NavItem>
-          <NavItem>
-            <Nav.Link tag={Link} to={'/page-2'}>
-              Projects
-            </Nav.Link>
-          </NavItem>
-          <NavItem>
-            <Nav.Link tag={Link} to={'/page-3'}>
-              page 3
-            </Nav.Link>
-          </NavItem>
-          <NavItem>
-            <Nav.Link tag={Link} to={'/page-4'}>
-              page 4
-            </Nav.Link>
-          </NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
-}
+    <div>
+      <Navbar className="mobile-nav" color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/about">About Me</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/projects">Projects</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
